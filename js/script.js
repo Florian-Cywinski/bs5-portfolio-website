258de48d63bb78ded895db5a78e89de2d22a11fc
Florian-Cywinski <florian.cywinski@protonmail.com>
2024-04-11 18:43:10 +0200

    Typing Effect JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // Add custom JavaScript here
+function runTypingEffect() {
+    const text = 'I am Brad Traversy.';
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 100;    // To have a delay of 100 ms per type
+  
+    typeText(text, typingElement, typingDelay);
+}
+  
+function typeText(text, typingElement, delay) {
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+        typingElement.textContent += text.charAt(i);    // To add each letter to the DOM
+        }, delay * i);
+    }
+}
+  
+document.addEventListener('DOMContentLoaded', runTypingEffect);
